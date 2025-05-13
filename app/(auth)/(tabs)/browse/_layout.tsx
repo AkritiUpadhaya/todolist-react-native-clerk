f685485a1fb3260217d8be32f3f57706486ba08d
AkritiUpadhaya <upadhayaakriti23@gmail.com>
2025-05-13 22:39:27 +0545

    updated browse screens outline

--- app/(auth)/(tabs)/browse/_layout.tsx
+++ app/(auth)/(tabs)/browse/_layout.tsx
@@ -1,8 +1,46 @@
+import { useUser } from '@clerk/clerk-expo'
+import { Ionicons } from '@expo/vector-icons'
 import { Stack } from 'expo-router'
 import React from 'react'
+import { Image, StyleSheet } from 'react-native'
 
-export default function BrowseLayout() {
+export default function browseLayout() {
   return (
-    <Stack/>
+    <Stack screenOptions={{headerShadowVisible:false, contentStyle:{backgroundColor:'#fff'}}}>
+      <Stack.Screen name="index" 
+      options={{ 
+        title:'Browse', 
+        headerLeft:({})=>{
+          return(
+            <HeaderLeft/>
+          )
+        },
+        headerRight:({})=>{
+          return(
+            <HeaderRight/>
+          )
+        }
+    }}/>
+    </Stack>
   )
 }
+
+const HeaderLeft = () => {
+  const {user}= useUser();
+    return<Image source={{uri: user?.imageUrl}} style= {styles.image}/>
+}
+
+const HeaderRight = () => {
+    return(
+        <Ionicons name="settings-outline" size={24} color="black"/>
+    )
+}
+
+const styles = StyleSheet.create({
+    image:{
+        width:32,
+        height:32,
+        borderRadius:16,
+        
+    }
+})
